9b69d3e490939d2cdad349bac7f13bb28cd62807
ysr-otn <yoshihiro.ohtani@gmail.com>
2016-06-25 21:47:20 +0900

    * ace-window の設定を追加

--- .emacs.d/init/init-ace-jump.el
+++ .emacs.d/init/init-ace-jump.el
@@ -1,6 +1,7 @@
 ;;; キー設定
 (define-key global-map (kbd "C-'") 'ace-jump-mode)
 
+
 ;;; 日本語対応のために ace-pinyin をロード
 (require 'ace-pinyin)
 
@@ -23,3 +24,9 @@
 ;;; キー設定
 (global-set-key (kbd "C-;") 'ace-jump-char-mode)
 (ace-pinyin-global-mode 1)
+
+
+;;; ace-window の設定
+(global-set-key (kbd "C-x o") 'ace-window)
+;; 1, 2, 3, ... の代りに a, s, d, ... を使用
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
