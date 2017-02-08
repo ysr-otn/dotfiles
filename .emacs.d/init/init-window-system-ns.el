d0f005228b3cc01252eb29115742fc3883bc5d37
ysr-otn <yoshihiro.ohtani@gmail.com>
2017-02-08 13:01:56 +0900

    * Mac の emacs で新しいフレームを作った時のフォント設定を修正

--- .emacs.d/init/init-window-system-ns.el
+++ .emacs.d/init/init-window-system-ns.el
@@ -28,10 +28,42 @@
 (set-face-background 'secondary-selection "white")
 
 ;;; フォントの設定
+
 ;;; 等幅フォントにする
 ;;; http://macemacsjp.sourceforge.jp/index.php?CocoaEmacs#j9873729
 (set-default-font
  "-*-Osaka-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+;; 新しいフレームを作ったときに同じフォントを使う
+(setq frame-inherited-parameters '(font tool-bar-lines))
+
+;; サイズは大きいが等幅フォントで使える
+;; http://d.hatena.ne.jp/kazu-yamamoto/20140625/1403674172
+;;
+;; 以下が Mac 用のフォント設定
+; (when (memq window-system '(mac ns))
+;   (global-set-key [s-mouse-1] 'browse-url-at-mouse)
+;   (let* ((size 14)
+; 	 (jpfont "Hiragino Maru Gothic ProN")
+; 	 (asciifont "Monaco")
+; 	 (h (* size 10)))
+;     (set-face-attribute 'default nil :family asciifont :height h)
+;     (set-fontset-font t 'katakana-jisx0201 jpfont)
+;     (set-fontset-font t 'japanese-jisx0208 jpfont)
+;     (set-fontset-font t 'japanese-jisx0212 jpfont)
+;     (set-fontset-font t 'japanese-jisx0213-1 jpfont)
+;     (set-fontset-font t 'japanese-jisx0213-2 jpfont)
+;     (set-fontset-font t '(#x0080 . #x024F) asciifont))
+;   (setq face-font-rescale-alist
+; 	'(("^-apple-hiragino.*" . 1.2)
+; 	  (".*-Hiragino Maru Gothic ProN-.*" . 1.2)
+; 	  (".*osaka-bold.*" . 1.2)
+; 	  (".*osaka-medium.*" . 1.2)
+; 	  (".*courier-bold-.*-mac-roman" . 1.0)
+; 	  (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+; 	  (".*monaco-bold-.*-mac-roman" . 0.9)
+; 	  ("-cdac$" . 1.3)))
+;   ;; C-x 5 2 で新しいフレームを作ったときに同じフォントを使う
+;   (setq frame-inherited-parameters '(font tool-bar-lines)))
 
 
 ;;; 綺麗ではあるが等幅フォントの設定がうまくいかなかったので無効化
