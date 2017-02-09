8ade00a4eb67d8ce0a246a0bedaf853c3064bfa8
ysr-otn <yoshihiro.ohtani@gmail.com>
2017-02-10 01:23:46 +0900

    * C-x C-j で skk-mode ではなく dired-x が起動する問題を修正

--- .emacs.d/init/init-ddskk.el
+++ .emacs.d/init/init-ddskk.el
@@ -7,6 +7,10 @@
   ;; .skk を自動的にバイトコンパイル
   (setq skk-byte-compile-init-file t))
 
+;; dired-x で使用している C-x C-j を skk で使用するように上書き
+(when (require 'dired-x nil t)
+  (global-set-key "\C-x\C-j" 'skk-mode))
+
 ;;; 送り仮名の自動認識
 ;(setq skk-auto-okuri-process nil)
 ;(setq skk-process-okuri-early nil)
