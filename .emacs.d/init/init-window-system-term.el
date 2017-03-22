c7b3de7bf6b901534e94589df1867cfb651c68cd
ysr-otn <yoshihiro.ohtani@gmail.com>
2017-03-22 17:36:01 +0900

    - Emacs のターミナル時のカラーテーマを設定

--- .emacs.d/init/init-window-system-term.el
+++ .emacs.d/init/init-window-system-term.el
@@ -1,3 +1,17 @@
+;;; 見易いカラーテーマを設定
+(load-theme 'tsdh-dark t)
+
 ;;; 背景色と文字色を設定
 (set-foreground-color "white")
 (set-background-color "black")
+
+(setq default-frame-alist
+					(append (list
+							 '(foreground-color . "white")
+							 '(background-color . "black")
+							 )))
+(setq initial-frame-alist 
+					(append (list
+							 '(foreground-color . "white")
+							 '(background-color . "black")
+							 )))
