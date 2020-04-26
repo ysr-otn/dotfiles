06623fbf1e7f3b61988e6a207390a5f478a02c53
ysr-otn <yoshihiro.ohtani@gmail.com>
2020-04-26 22:03:23 +0900

    - init-org-mode.el に ox-taskjuggler.el に関する著作権とライセンスを追記 - init-org-mode.el に GPL v3 のライセンスを追記

--- .emacs.d/init/init-org-mode.el
+++ .emacs.d/init/init-org-mode.el
@@ -1,3 +1,63 @@
+;;; init-org-mode.el --- Initialize settings for org-mode.
+
+;; Copyright (C) 2020 Yoshihiro Ohtani
+
+;; Author: Yoshihiro Ohtani
+
+;; The functions org-taskjuggler-get-start, org-taskjuggler--build-task are modified from
+;; the orignal functions these are implemented in ox-taskjuggler.el.
+;; More details of the copyright and the license about ox-taskjuggler.el, see below.
+
+	;;; ox-taskjuggler.el --- TaskJuggler Back-End for Org Export Engine
+	;;
+	;; Copyright (C) 2007-2020 Free Software Foundation, Inc.
+	;;
+	;; Emacs Lisp Archive Entry
+	;; Filename: ox-taskjuggler.el
+	;; Author: Christian Egli
+	;;      Nicolas Goaziou <n dot goaziou at gmail dot com>
+	;; Maintainer: Christian Egli
+	;; Keywords: org, taskjuggler, project planning
+	;; Description: Converts an Org mode buffer into a TaskJuggler project plan
+	
+	;; This file is not part of GNU Emacs.
+	
+	;; This program is free software: you can redistribute it and/or modify
+	;; it under the terms of the GNU General Public License as published by
+	;; the Free Software Foundation, either version 3 of the License, or
+	;; (at your option) any later version.
+	
+	;; This program is distributed in the hope that it will be useful,
+	;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+	;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	;; GNU General Public License for more details.
+	
+	;; You should have received a copy of the GNU General Public License
+	;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+
+;;; Code:
+	
+
 ;;; コードブロックをそのモードと同じ色付けをする
 (setq org-src-fontify-natively t)
 
@@ -89,7 +149,10 @@
 			   (load-library "ox-reveal")
 			   )
 			 
-			 ;; org-version 9.3.6 以降で org-taskjuggler-get-start, org-taskjuggler--build-task を上書き
+			 ;; org-version 9.3.6 以降で org-taskjuggler-get-start, org-taskjuggler--build-task を上書き．
+			 ;; (# でコメント部が オリジナルからの変更部分)
+			 ;; Overwrite org-taskjuggler-get-start, org-taskjuggler--build-task, if org-version is 9.3.6 or higher.
+			 ;; (Comments written with # are modified from the original version.)
 			 (if (string> org-version "9.3.5")
 				 (progn
 				   (require 'ox-taskjuggler)
@@ -109,7 +172,6 @@ doesn't have any start date defined."
 				   
 				   ;; org-taskjuggler--build-task でプロパティ :start: が無くとも :SCHEDULED の値で
 				   ;; TaskJuggleru の start 要素を設定できるように org-taskjuggler--build-task を上書き
-				   ;; (# でコメント部が オリジナルからの変更部分)
 				   (defun org-taskjuggler--build-task (task info)
 "Return a task declaration.
 
