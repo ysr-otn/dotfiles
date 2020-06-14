a0e2841035b075628812d14584b61c4748872d98
ysr-otn <yoshihiro.ohtani@gmail.com>
2020-06-14 14:11:32 +0900

    - slime-mode のキー設定を追加

--- .emacs.d/init/init-slime.el
+++ .emacs.d/init/init-slime.el
@@ -5,3 +5,8 @@
 ;; SLIMEのロード
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner)) 
+
+;;; slime-mode で上書きされるキー設定を元に戻す
+(define-key slime-mode-map (kbd "M-?") 'help-for-help)
+(define-key slime-mode-map (kbd "M-p") 'previous-line-more)
+(define-key slime-mode-map (kbd "M-n") 'next-line-more)
