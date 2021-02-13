801718a11aab1c443ceda350a3be40aeadb2bccb
ysr-otn <yoshihiro.ohtani@gmail.com>
2021-02-13 11:51:27 +0900

    - google-translate で tkk のエラーが出るのを修正

--- .emacs.d/init/init-google-translate.el
+++ .emacs.d/init/init-google-translate.el
@@ -38,3 +38,32 @@ translation it is possible to get suggestion."
     (if (and info (> (length info) 0))
         (aref info 1)
       nil)))    
+
+
+;;; tkk 関係のエラーが出る問題への暫定対策
+;;; https://qiita.com/minoruGH/items/75eb4fab53e93653f999
+;;; https://github.com/atykhonov/google-translate/issues/52#issuecomment-423742883
+(defun google-translate-enja-or-jaen (&optional string)
+  "Translate words in region or current position. Can also specify query with C-u"
+  (interactive)
+  (setq string
+        (cond ((stringp string) string)
+              (current-prefix-arg
+               (read-string "Google Translate: "))
+              ((use-region-p)
+               (buffer-substring (region-beginning) (region-end)))
+              (t
+               (thing-at-point 'word))))
+  (let* ((asciip (string-match
+                  (format "\\`[%s]+\\'" "[:ascii:]’“”–")
+                  string)))
+    (run-at-time 0.1 nil 'deactivate-mark)
+    (google-translate-translate
+     (if asciip "en" "ja")
+     (if asciip "ja" "en")
+     string)))
+
+;; Fix error of "Failed to search TKK"
+(defun google-translate--get-b-d1 ()
+    ;; TKK='427110.1469889687'
+  (list 427110 1469889687))
