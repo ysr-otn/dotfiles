9ce688b5b4735c9e42e390ec441cdf91a6e02c02
ysr-otn <yoshihiro.ohtani@gmail.com>
2022-01-22 22:53:45 +0900

    - fish に Terminal, lang, Editor, PATH, Emacs Environment, C/C++, Python の設定を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+####### Terminal #######
+set -x TERM "xterm-256color"	# 256 色表示のターミナルに対応
+
+
 ####### Set Host #######
 set -x HOSTNAME (hostname)
 
@@ -15,6 +19,64 @@ else if [ $HOSTNAME = PC-PA1710C2120R -a (uname -s) = Linux ]
 else
 	set -x HOSTTYPE i386		# mac
 end
+
+
+####### Set lang #######
+if [ $HOSTTYPE = linux ]
+	set -x LANG ja_JP.eucJP
+else
+	set -x LANG ja_JP.UTF-8
+end
+	
+###### Set Editor ######
+if [ $HOSTTYPE != linux ]			# Linux 環境では vim の日本語設定が正しく動作しないのでエディタに vim は設定しない
+	set EDITOR vim
+end
+
+########## PATH ##########
+set -x PATH $HOME/Tools/$HOSTTYPE/bin:$HOME/Tools/share/bin:$PATH
+if [ $HOSTTYPE = linux ]
+	set -x PATH $HOME/pkg/bin:$HOME/pkg/sbin:$PATH
+end
+set -x MANPATH $HOME/Tools/$HOSTTYPE/man:$MANPATH
+
+set -x GITHUB_DOTFILE_DIR $HOME/Development/github/ysr-otn/dotfiles
+
+# llvm 関係のパス設定
+if [ $HOSTTYPE = i386 ]
+	set PATH /usr/local/opt/llvm/bin:$PATH
+	set DYLD_LIBRARY_PATH /usr/local/opt/llvm/lib:$DYLD_LIBRARY_PATH 
+	set LDFLAGS "-L/usr/local/opt/llvm/lib"
+	set CPPFLAGS "-I/usr/local/opt/llvm/include"	
+else if [ $HOSTTYPE = windows ]
+	set PATH /cygdrive/c/msys64/mingw64/bin:$PATH
+	set DYLD_LIBRARY_PATH /cygdrive/c/msys64/mingw64/lib:$DYLD_LIBRARY_PATH 
+	set LDFLAGS "-L/cygdrive/c/msys64/mingw64/lib"
+	set CPPFLAGS "-I/cygdrive/c/msys64/mingw64/include"	
+end
+
+####### Emacs Environment ########
+set EMACS_DIR $HOME/.inits/share/emacs
+set EMACS_STARTUP $EMACS_DIR/startup
+set EMACS_SITE_LISP $HOME/Tools/$HOSTTYPE/share/emacs/site-lisp
+set EMACS_INFO $HOME/Tools/$HOSTTYPE/share/info
+set EMACS_INFO2 $HOME/Tools/$HOSTTYPE/info
+
+#######	C/C++ の設定  #######
+if [ $HOSTTYPE = i386 ]
+	# Catalina 以降の Mac での stdio.h のインクルードエラー対策
+	set SDKROOT (xcrun --sdk macosx --show-sdk-path)
+end
+
+#######	Python の設定  #######
+if [ $HOSTTYPE = i386 -o $HOSTTYPE = ubuntu ]
+	set PYENV_ROOT "$HOME/.pyenv"
+	set PATH "$PYENV_ROOT/bin:$PATH"
+	pyenv init - | source
+else if [ $HOSTTYPE = windows ]
+	set PATH "$HOME/AppData/Local/Programs/Python/Python39:$HOME/AppData/Local/Programs/Python/Python39/Scripts:$PATH"
+end
+
 
 
 #######	Aliasis  #######
