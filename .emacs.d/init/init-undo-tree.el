68dfb7a7d7a900bdaf9a7a8131c5ae9adb9fa781
ysr-otn <yoshihiro.ohtani@gmail.com>
2022-08-18 14:31:03 +0900

    - undo-tree のファイルを置くディレクトリの指定を追加

--- .emacs.d/init/init-undo-tree.el
+++ .emacs.d/init/init-undo-tree.el
@@ -1,3 +1,6 @@
 (require 'undo-tree)
 (global-undo-tree-mode t)
 ;(global-set-key (kbd "M-/") 'undo-tree-redo)
+
+;;; undo-tree のファイルを置くディレクトリ
+(setq undo-tree-history-directory-alist `(("." . "~/.emacs.d/undohist")))
