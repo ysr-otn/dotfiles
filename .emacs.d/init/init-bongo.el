3728c4d2b3013ebf958053d6afb1f790187bf687
ysr-otn <yoshihiro.ohtani@gmail.com>
2025-06-15 15:39:27 +0900

    - Emacs の Bongo に Timidity++ で MIDI を再生する設定を追加

--- .emacs.d/init/init-bongo.el
+++ .emacs.d/init/init-bongo.el
@@ -1,8 +1,10 @@
-;;; https://pxaka.tokyo/wiki/emacs#bongo_%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B
 ;;; bongo が使用するプレーヤとファイルの設定
 (cond ((eq system-type 'darwin)					; Mac の場合は afplay
 	   (progn
 		 (setq bongo-custom-backend-matchers
 			   '((afplay local-file "mp3")
-				 (afplay local-file "m4a")))))
-		 )
+				 (afplay local-file "m4a")
+				 (timidity local-file "mid")
+				 (timidity local-file "midi")))
+		 (setq bongo-timidity-extra-arguments "-Od")	; Timidity 再生のために -Od オプションが必要
+		 )))
